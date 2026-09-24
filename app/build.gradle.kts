d1f4cf4cd90ff530a9524286e3efec0fe8411260
Wassou Kemlong <emmanuelemmanueljunior6@gmail.com>
2026-09-24 19:16:35 +0100

    Third commit, after last modifications

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -92,9 +92,7 @@ dependencies {
   androidTestImplementation(libs.androidx.test.espresso.core)
 
     // Navigation
-    implementation(libs.androidx.navigation3.ui)
-    implementation(libs.androidx.navigation3.runtime)
-    implementation(libs.androidx.lifecycle.viewmodel.navigation3)
+    implementation(libs.androidx.navigation.compose)
 // Room
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
